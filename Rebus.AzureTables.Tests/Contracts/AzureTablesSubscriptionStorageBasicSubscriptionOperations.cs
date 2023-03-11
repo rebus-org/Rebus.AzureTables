ded8e27cbc68bb276a510e783bb9aeae36fac03b
Anderson Miranda <anderson@fastchannel.com>
2023-03-11 18:36:31 +0000

    Adding Azure Tables as Subscription Storage (thanks to @jakubbohm)

---
+++ Rebus.AzureTables.Tests/Contracts/AzureTablesSubscriptionStorageBasicSubscriptionOperations.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Subscriptions;
+
+namespace Rebus.AzureTables.Tests.Contracts
+{
+    [TestFixture]
+    public class AzureTablesSubscriptionStorageBasicSubscriptionOperations : BasicSubscriptionOperations<AzureTablesSubscriptionStorageFactory>
+    {
+    }
+}
